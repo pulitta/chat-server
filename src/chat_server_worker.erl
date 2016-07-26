7dbb280b0ff3cb1a5d9e894c79b52c67cf9e8e37
pulitta <pulitta11@gmail.com>
2016-07-27 01:52:16 +0300

    worker added

---
+++ src/chat_server_worker.erl
@@ -0,0 +1,35 @@
+-module(chat_server_worker).
+
+-behaviour(gen_server).
+
+-export([
+    init/1, 
+    handle_call/3, 
+    handle_cast/2, 
+    handle_info/2, 
+    terminate/2, 
+    code_change/3
+]).
+
+-export([start_link/1]).
+
+start_link(Args) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
+
+init(Args) ->
+    {ok, ok}.
+
+handle_call(Request, _From, State) ->
+    {stop, {unknown_call, Request}, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
